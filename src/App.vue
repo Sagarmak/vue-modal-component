b841099320983d518a69b3f171b32753f41c3f03
Sagar Makhija <sagar1309@live.com>
2020-06-26 22:12:29 +0530

    completes Modal functionality

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="app">
+    <div class="img vue-image">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+    <div class="button">
+      <button class="default" @click="showModal=!showModal">Click here to enable modal</button>
+    </div>
+
+
+    <!-- Modal -->
+    <ModalComponent 
+      v-if="showModal"
+      :title="modalTitle"
+      :body="modalBody"
+      :height="'50rem'"
+      :width="'50rem'"
+      :successActionButtonTitle="successActionButtonTitle"
+      :cancelActionButtonTitle="cancelActionButtonTitle"
+      @modal-component="modalComponentActionHandler" 
+    >
+      <!-- <template v-slot:body>
+        Slot Body
+      </template> -->
+    </ModalComponent>
+
+    <div class="form">
+      <div class="form-item form-title">
+        <p>Enter Modal Title:</p>
+        <input v-model="modalTitle" type="text" name="modalTitle" placeholder="Enter modal title here...">
+      </div>
+      <div class="form-item form-body">
+        <p>Enter Modal Body:</p>
+        <textarea v-model="modalBody" name="modalBody" placeholder="Enter modal body here..." cols="30" rows="10"></textarea>
+      </div>
+      <div class="form-item form-success-button">
+        <p>Enter Modal Success Button Name:</p>
+        <input v-model="successActionButtonTitle" type="text" name="successActionButtonTitle" placeholder="Enter modal success button name here...">
+      </div>
+      <div class="form-item form-cancel-button">
+        <p>Enter Modal Cancel Button Name:</p>
+        <input v-model="cancelActionButtonTitle" type="text" name="cancelActionButtonTitle" placeholder="Enter modal cancel button name here...">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ModalComponent from './components/ModalComponent';
+export default {
+  name: 'App',
+  components: {
+    ModalComponent
+  },
+  data() {
+    return {
+      showModal: false,
+      modalTitle: `Modal title as a prop`,
+      modalBody: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+      successActionButtonTitle: 'Success',
+      cancelActionButtonTitle: 'Close'
+    };
+  },
+  methods: {
+    modalComponentActionHandler(e) {
+      if(!e) {
+        // close modal
+        this.showModal = false;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  vertical-align: baseline;
+  box-sizing: border-box;
+}
+
+body {
+  background: #3c369a;
+  color: white;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 1rem;
+}
+.img, .button, pre {
+  text-align: center;
+}
+button.default {
+  margin: 1rem 0;
+  border-radius: 5px;
+  border: 1px solid black;
+  padding: 0.5rem 1rem;
+  background: #241f73;
+  color: white;
+  cursor: pointer;
+}
+.form {
+  margin: auto;
+  width: 20%;
+}
+.form .form-item {
+  margin-top: 1rem;
+  font-family: monospace;
+}
+.form .form-item p {
+  text-align: left;
+  padding: 0.5rem 0;
+}
+.form .form-item input, textarea {
+  width: 115%;
+  padding: 0.5rem;
+  border-radius: 6px;
+}
+</style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="modal-component">
+    <div class="modal">
+      <div class="modal-content">
+        <div class="header">
+          <span class="title">{{ title }}</span>
+          <button @click="$emit('modal-component', false)">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+        <div class="body">
+          <div v-if="body">
+            {{ body }}
+          </div>
+          <div>
+            <slot name="body"></slot>
+          </div>
+        </div>
+        <div class="footer">
+          <button> {{ successActionButtonTitle }} </button>
+          <button @click="$emit('modal-component', false)"> {{ cancelActionButtonTitle }} </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalComponent',
+  props: {
+    title: {
+      type: String,
+      default: 'Modal Title',
+      validator: (value) => value.length > 0
+    },
+    body: {
+      type: String
+    },
+    height: {
+      type: String,
+      default: '100%'
+    },
+    width: {
+      type: String,
+      default: '80%'
+    },
+    successActionButtonTitle: {
+      type: String,
+      default: 'Ok'
+    },
+    cancelActionButtonTitle: {
+      type: String,
+      default: 'Cancel'
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  background: transparent;
+  cursor: pointer;
+}
+.modal {
+  color: black;
+  text-align: left;
+
+  position: fixed;
+  z-index: 1;
+  background-color: rgba(0,0,0,0.4);
+  
+  overflow: auto;
+
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+.modal-content {
+  background: #fefefe;
+  width: 80%;
+  margin: 2rem auto;
+  /* padding: 2rem; */
+}
+.modal-content .header {
+  padding: 1.5rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: solid 1px whitesmoke;
+}
+.modal-content .body {
+  padding: 2rem;
+  border-bottom: solid 1px whitesmoke;
+}
+.modal-content .footer {
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.modal-content .footer button {
+  background: whitesmoke;
+  padding: 0.5rem 1rem;
+  border-radius: 5%;
+  box-shadow: 2px 2px 5px -1px rgba(0,0,0,0.25);
+}
+</style>
